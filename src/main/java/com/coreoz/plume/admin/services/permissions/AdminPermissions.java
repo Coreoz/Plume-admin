0f5c2a3f74ab96ff7db1a564c55cdd69da352b45
Aurélien Manteaux <amanteaux@gmail.com>
2016-08-15 17:26:59 +0200

    Add a generic permission

--- src/main/java/com/coreoz/plume/admin/services/permissions/AdminPermissions.java
+++ src/main/java/com/coreoz/plume/admin/services/permissions/AdminPermissions.java
@@ -5,5 +5,7 @@ public class AdminPermissions {
 	public static final String MANAGE_USERS = "MANAGE_USERS";
 	public static final String SEE_ROLES = "SEE_ROLES";
 	public static final String MANAGE_ROLES = "MANAGE_ROLES";
+	// if only one permission is enough for customization
+	public static final String GENERIC_ACCESS = "GENERIC_ACCESS";
 
 }
