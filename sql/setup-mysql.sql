56deb2f9cb892d870e81a115df6a29c17f1835a8
Aurélien Manteaux <amanteaux@coreoz.com>
2016-10-12 10:11:59 +0200

    Update mysql setup script

--- sql/setup-mysql.sql
+++ sql/setup-mysql.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS `plm_role`;
-CREATE TABLE  `plm_role` (
+DROP TABLE IF EXISTS `PLM_ROLE`;
+CREATE TABLE  `PLM_ROLE` (
   `id` bigint(20) NOT NULL,
   `label` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `uniq_plm_role_label` (`label`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `plm_user`;
-CREATE TABLE  `plm_user` (
+DROP TABLE IF EXISTS `PLM_USER`;
+CREATE TABLE  `PLM_USER` (
   `id` bigint(20) NOT NULL,
   `id_role` bigint(20) NOT NULL,
   `creation_date` datetime NOT NULL,
@@ -19,20 +19,20 @@ CREATE TABLE  `plm_user` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `uniq_plm_user_email` (`email`),
   UNIQUE KEY `uniq_plm_user_username` (`user_name`),
-  CONSTRAINT `plm_user_role` FOREIGN KEY (`id_role`) REFERENCES `plm_role` (`id`)
+  CONSTRAINT `plm_user_role` FOREIGN KEY (`id_role`) REFERENCES `PLM_ROLE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `plm_role_permission`;
-CREATE TABLE  `plm_role_permission` (
+DROP TABLE IF EXISTS `PLM_ROLE_PERMISSION`;
+CREATE TABLE  `PLM_ROLE_PERMISSION` (
   `id_role` bigint(20) NOT NULL,
   `permission` varchar(255) NOT NULL,
   PRIMARY KEY (`id_role`, `permission`),
-  CONSTRAINT `plm_role_permission_role` FOREIGN KEY (`id_role`) REFERENCES `plm_role` (`id`)
+  CONSTRAINT `plm_role_permission_role` FOREIGN KEY (`id_role`) REFERENCES `PLM_ROLE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO plm_role VALUES(1, 'Administrator');
-INSERT INTO plm_user VALUES(1, 1, NOW(), 'Admin', 'Admin', 'admin@admin', 'admin', '$2a$11$FfgtfoHeNo/m9jGj9D5rTO0zDDI4LkMXnXHai744Ee32P3CHoBVqm');
-INSERT INTO plm_role_permission VALUES(1, 'MANAGE_USERS');
-INSERT INTO plm_role_permission VALUES(1, 'MANAGE_ROLES');
-INSERT INTO plm_role_permission VALUES(1, 'GENERIC_ACCESS');
+INSERT INTO PLM_ROLE VALUES(1, 'Administrator');
+INSERT INTO PLM_USER VALUES(1, 1, NOW(), 'Admin', 'Admin', 'admin@admin', 'admin', '$2a$11$FfgtfoHeNo/m9jGj9D5rTO0zDDI4LkMXnXHai744Ee32P3CHoBVqm');
+INSERT INTO PLM_ROLE_PERMISSION VALUES(1, 'MANAGE_USERS');
+INSERT INTO PLM_ROLE_PERMISSION VALUES(1, 'MANAGE_ROLES');
+INSERT INTO PLM_ROLE_PERMISSION VALUES(1, 'GENERIC_ACCESS');
