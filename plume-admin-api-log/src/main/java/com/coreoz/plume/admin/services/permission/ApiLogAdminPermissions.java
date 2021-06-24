c68a409e2c3cb9dcda7f7c691fa9aaf3e5725765
Aurélien Manteaux <amanteaux@coreoz.com>
2021-06-24 13:35:09 +0200

    Leave old apiLog permission to avoid breaking change

---
+++ plume-admin-api-log/src/main/java/com/coreoz/plume/admin/services/permission/ApiLogAdminPermissions.java
@@ -0,0 +1,9 @@
+package com.coreoz.plume.admin.services.permission;
+
+/**
+ * @deprecated Use {@link LogApiAdminPermissions} instead. Will be deleted in Plume 3.0.0
+ */
+@Deprecated
+public class ApiLogAdminPermissions {
+    public static final String MANAGE_API_LOGS = "MANAGE_API_LOGS";
+}
