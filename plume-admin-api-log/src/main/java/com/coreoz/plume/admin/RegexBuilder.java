999c11845ba2f7c9c541c4ee25578515f635d313
Jérémy Fardilha <jfardilha@coreoz.com>
2023-07-04 18:30:16 +0200

    Add url regex list predicate

---
+++ plume-admin-api-log/src/main/java/com/coreoz/plume/admin/RegexBuilder.java
@@ -0,0 +1,15 @@
+package com.coreoz.plume.admin;
+
+import java.util.List;
+
+public class RegexBuilder {
+    static String computeUrlRegexList(List<String> urlRegexList) {
+        return urlRegexList.stream()
+            .reduce("", (currentRegex, element) -> {
+                if (currentRegex.isEmpty()) {
+                    return "(" + element + ")";
+                }
+                return currentRegex + "|(" + element + ")";
+            });
+    }
+}
